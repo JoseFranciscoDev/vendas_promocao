9b395edc3719b7a5a55a7ef08a62f53087ebb0d1
JoseFranciscoDev <josefranciscodevpi@gmail.com>
2026-08-30 15:52:23 -0300

    feat: adiciona os códigos da planilha online na query sql para posteriomente ser executada pelo mysql-connector

--- vendas_produtos_promocao.py
+++ vendas_produtos_promocao.py
@@ -1,23 +1,32 @@
-import os
+from os import getenv
 
 import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv()
+with open("./query.sql") as q:
+    query_sql = q.read()
 
-# sheet_id = os.getenv("sheet_id")
-# gid = os.getenv("gid")
+sheet_id = getenv("sheet_id")
+gid = getenv("gid")
 
-# url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
 
-# df = (
-#     pd.read_csv(url, skiprows=3, header=0)
-#     .dropna(how="all")["COD PROD."]
-#     .dropna()
-#     .astype(int)
-# )
+df = (
+    pd.read_csv(url, skiprows=3, header=0)
+    .dropna(how="all")["COD PROD."]
+    .dropna()
+    .astype(int)
+)
 
-# codes_list = df.tolist()
+code_list = df.tolist()
+first_metad_query = query_sql[:1135]
+second_metad_query = query_sql[1135:]
 
-query = os.getenv("query_sql")
-print(query)
+for codigo in code_list:
+    if code_list.index(codigo) == len(code_list) - 1:
+        first_metad_query += str(codigo)
+        break
+    first_metad_query += str(codigo) + ","
+
+full_query = first_metad_query + second_metad_query
